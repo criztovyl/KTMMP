ccd2244ceed8193722471316de1f13682e3d33b3
criztovyl <ch.schulz@joinout.de>
2014-12-01 21:01:54 +0100

    Adds new boxes, does some CSS arrangements.

--- js.js
+++ js.js
@@ -8,12 +8,6 @@ function init(){
             [0        , "osi"       , "heutemorgen", 0]
             ]);
 
-    /*boxManager.add(1, new Box('1', 'Box 1', "This is Box 1:)"));
-    boxManager.add(2, new Box('2', 'Box 2', "This is Box 2:)"));
-    boxManager.add(3, new Box('3', 'Box 3', "This is Box 3:)"));
-    boxManager.add(4, new Box('4', 'Box 4', "This is Box 4:)"));
-    boxManager.add(5, new Box('5', 'Box 5', "This is Box 5:)"));
-    boxManager.add(6, new Box('6', 'Box 6', "This is Box 6:)"));*/
     boxManager.add("tech", new Box('tech', 'Technik', "tech.html", true));
     boxManager.add("http", new Box('http', 'HTTP', 'http.html', true));
     boxManager.add("inetprotos", new Box('inetprotos', 'Die Internetprotokollfamilie', 'internetprotocolfamily.html', true));
@@ -25,9 +19,9 @@ function init(){
     boxManager.add("osi", new Box("osi", "OSI Modell", "osi.html", true));
     boxManager.add("protos", new Box("protos", "Protokolle", "protos.html", true));
     boxManager.add("html", new Box("html", "HTML", "html.html", true));
-    boxManager.add("hist", new Box("hist", "Geschichte", "hist.html", true));
-    boxManager.add("arpa", new Box("arpa", "ARPANET", "arpa.html", true));
-    boxManager.add("heutemorgen", new Box("heutemorgen", "Heute &amp; Morgen", "heutemorgen.html", true));
+//    boxManager.add("hist", new Box("hist", "Geschichte", "hist.html", true));
+//    boxManager.add("arpa", new Box("arpa", "ARPANET", "arpa.html", true));
+//    boxManager.add("heutemorgen", new Box("heutemorgen", "Heute &amp; Morgen", "heutemorgen.html", true));
 
     boxManager.print();
 //    style();
